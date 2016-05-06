52f108919f529084fb3e8171bff72a807b1d38ea
AfrikaFleet <admin@afrikafleet.com>
2016-05-06 16:40:02 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Innocent D. Wabo
+#    Copyright 2016 AfrikaFleet SARL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from ./models import contract_type
+from ./models import device_type
+from ./models import event_type
+from ./models import gsm_plan_periodicity
+from ./models import gsm_pan_services
+from ./models import license_type
+from ./models import object_type
+from ./models import device
+from ./models import device_assignation
+from ./models import device_manufacturer
+from ./models import device_model
+from ./models import driver
+from ./models import driver_assignation
+from ./models import event
+from ./models import gsm_plan
+from ./models import gsm_provider
+from ./models import gsm_sim
+from ./models import gsm_sim_plan
+from ./models import license_assignation
+from ./models import location
+from ./models import server_address_port
+from ./models import server_license
+from ./models import server_provider
+from ./models import site
+from ./models import site_admin
+from ./models import tracked_object
+from ./models import tracking_account
